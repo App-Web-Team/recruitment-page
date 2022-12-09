09a7c1dce1522261884180ddc3a84bfe19e73f81
md-ameen <mdameenbf@gmail.com>
2022-12-09 17:49:27 +0530

    jobs data fetched from db

--- assets/scripts/recruitment.js
+++ assets/scripts/recruitment.js
@@ -1,8 +1,15 @@
 $("#btn-1").click(function () {
     $(".container > div:nth-child(2) p").removeClass("hide-jd");
     $(".container > div:nth-child(4) p").addClass("hide-jd");
+    $(".container > div:nth-child(6) p").addClass("hide-jd");
 });
 $("#btn-2").click(function () {
     $(".container > div:nth-child(2) p").addClass("hide-jd");
     $(".container > div:nth-child(4) p").removeClass("hide-jd");
+    $(".container > div:nth-child(6) p").addClass("hide-jd");
+});
+$("#btn-3").click(function () {
+    $(".container > div:nth-child(2) p").addClass("hide-jd");
+    $(".container > div:nth-child(4) p").addClass("hide-jd");
+    $(".container > div:nth-child(6) p").removeClass("hide-jd");
 });
--- sections/header-section.php
+++ sections/header-section.php
@@ -3,54 +3,59 @@
 </head>
 
 <div class="container header-container py-3">
-    <div class="row text-center text-sm-left justify-content-center justify-content-sm-between mt-4">
-        <div class="col-10 col-xs-5 col-sm-6">
-            <button id="btn-1" class="job-title text-capitalize">
-                graphic designer
-            </button>
-            <p class="job-id text-uppercase">
-                Job ID: JPW22D-GD-1001
-            </p>
-        </div>
-        <div class="col-10 col-xs-5 col-sm-6 mt-3 my-xs-auto d-sm-flex justify-content-sm-end">
-            <a href="#apply-section">
-                <button class="btn btn-danger text-capitalize">
-                    apply now
-                </button>
-            </a>
-        </div>
-    </div>
-    <div class="row jd-row justify-content-center">
-        <div class="col-10 col-sm-12">
-            <p class="job-desc mt-4">
-                We are looking for a rock star Graphics Designer/Motion graphics for our team in Bangalore GlowRoad(a unit of Amazon), India, who will work on different Cross-channel/Cross-site marketing projects, all aimed at supporting marketing campaigns and offering clients compelling and engaging visual experiences. The candidate should be a learning expert who is creative, analytical and able to coordinate a small team of graphic designers working on the same project.
-            </p>
-        </div>
-    </div>
-    <div class="row text-center text-sm-left justify-content-center justify-content-sm-between mt-4">
-        <div class="col-10 col-xs-5 col-sm-6">
-            <button id="btn-2" class="job-title text-capitalize">
-                Web Developer
-            </button>
-            <p class="job-id text-uppercase">
-                Job ID: JPW22I-WD-1001
-            </p>
-        </div>
-        <div class="col-10 col-xs-5 col-sm-6 mt-3 my-xs-auto d-sm-flex justify-content-sm-end">
-            <a href="#apply-section">
-                <button class="btn btn-danger text-capitalize">
-                    apply now
-                </button>
-            </a>
-        </div>
-    </div>
-    <div class="row jd-row justify-content-center">
-        <div class="col-10 col-sm-12">
-            <p class="job-desc mt-4 hide-jd">
-                We are looking for a rock star Graphics Designer/Motion graphics for our team in Bangalore GlowRoad(a unit of Amazon), India, who will work on different Cross-channel/Cross-site marketing projects, all aimed at supporting marketing campaigns and offering clients compelling and engaging visual experiences. The candidate should be a learning expert who is creative, analytical and able to coordinate a small team of graphic designers working on the same project.
-            </p>
-        </div>
-    </div>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "career-apply";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM jobs";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            // echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
+            echo '
+            <div class="row text-center text-sm-left justify-content-center justify-content-sm-between mt-4">
+                <div class="col-10 col-xs-5 col-sm-6">
+                    <button id="btn-1" class="job-title text-capitalize">
+                    '. $row["jobRole"] .'
+                    </button>
+                    <p class="job-id text-uppercase">
+                        Job ID: '. $row["jobId"] .'
+                    </p>
+                </div>
+                <div class="col-10 col-xs-5 col-sm-6 mt-3 my-xs-auto d-sm-flex justify-content-sm-end">
+                    <a href="#apply-section">
+                        <button class="btn btn-danger text-capitalize">
+                            apply now
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="row jd-row justify-content-center">
+                <div class="col-10 col-sm-12">
+                    <p class="job-desc mt-4">
+                    '. $row["jobDesc"] .'
+                    </p>
+                </div>
+            </div>
+            ';
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+    ?>
+    <!-- soon -->
     <div class="row mt-4 justify-content-center">
         <p class="end-text mb-5">
             Thats it for today. Will Update tomorrow
